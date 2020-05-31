177a723384050bc26f6020f9053d7c722558901f
zzh <zhengmurong168@sina.com>
2020-05-31 21:48:33 +0800

    更新路由

--- routers/router.go
+++ routers/router.go
@@ -16,14 +16,21 @@ func init() {
     beego.Router("/getAuth", &controllers.AuthController{})
     beego.Router("/getherodata", &controllers.GetHeroDataController{})
     beego.Router("/getheroesicon", &controllers.GetHeroIconController{})
-    beego.Router("/setplayerinfo/", &controllers.SetPlayerInfoController{})
+    beego.Router("/setplayerinfo/?:playerid", &controllers.SetPlayerInfoController{})
+    beego.Router("/gettenmindata/", &controllers.GetTenMinDataController{})
     //replay 上传录像
     beego.Router("/repupload/", &controllers.ReplayUploadController{})
     beego.Router("/leagueinfo/", &controllers.LeagueInofController{})
     beego.Router("/getleagueinfo/", &controllers.GetLeagueInofController{})
-    beego.Router("/teaminfo/",&controllers.TeamInofController{})
+    beego.Router("/saveteaminfo/",&controllers.SaveTeamInofController{})
     beego.Router("/getallteaminfo/",&controllers.AllTeamInofController{})
+    beego.Router("/getteamalldata/",&controllers.GetTeamInofController{})
     beego.Router("/getteamheropool",&controllers.TeamHeroPoolController{})
+    beego.Router("/getmatchdetails",&controllers.MatchDetailsController{})
+    beego.Router("/getmiduseheroes",&controllers.MidUseHeroesController{})
+    beego.Router("/getsidelineup",&controllers.SideLineUpController{})
+    beego.Router("/getplayersamehero",&controllers.PlayerSameHeroController{})
+
 
     beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
         AllowAllOrigins:  true,
